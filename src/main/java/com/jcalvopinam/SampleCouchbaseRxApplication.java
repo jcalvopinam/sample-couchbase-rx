7f2092a1b841dfd358dea107de50a185b0371356
Juan Calvopina <juan.calvopina@gmail.com>
2018-08-25 23:09:35 -0300

    Initial commit

---
+++ src/main/java/com/jcalvopinam/SampleCouchbaseRxApplication.java
@@ -0,0 +1,12 @@
+package com.jcalvopinam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCouchbaseRxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleCouchbaseRxApplication.class, args);
+    }
+}
